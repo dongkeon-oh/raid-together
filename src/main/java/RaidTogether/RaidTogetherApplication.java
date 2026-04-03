7e7ba278846ae42024c6dce0f32e8dd95237b909
Dongkeon Oh <dongkeon.oh@gmaile.com>
2026-04-03 16:58:18 +0900

    Initial Commit: raid-together

---
+++ src/main/java/RaidTogether/RaidTogetherApplication.java
@@ -0,0 +1,13 @@
+package RaidTogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaidTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaidTogetherApplication.class, args);
+	}
+
+}
